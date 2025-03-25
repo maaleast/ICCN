880a7a0fa4970c6a41b5731e9a5fbf63f49cdab1
Azmi Jalaluddin Amron <azmijalaluddin@gmail.com>
2025-03-25 12:48:44 +0700

    Ganti tempat notifikadi

--- src/App.jsx
+++ src/App.jsx
@@ -58,7 +58,7 @@ function App() {
   return (
     <Router>
       <ToastContainer 
-        position="top-right"
+        position="bottom-right"
         autoClose={5000}
         hideProgressBar={false}
         newestOnTop={false}
