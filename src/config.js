f57c4b8fd37022f982a8ba0f1e69a038115afcf1
Azmi Jalaluddin Amron <123649635+aam19azmi@users.noreply.github.com>
2025-06-06 04:40:14 +0700

    Update config.js
    Update hosting backend

--- src/config.js
+++ src/config.js
@@ -1,3 +1,3 @@
 // export const API_BASE_URL = "http://localhost:5050";
 
-export const API_BASE_URL = "https://iccnbackend-production-583c.up.railway.app";
+export const API_BASE_URL = "https://glitch.com/~apricot-cake-barge";
